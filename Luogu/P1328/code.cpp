f5b0dcac5500ea5aa29e52cf5a6727d0246df292
zrc4889 <zrc4889@163.com>
2021-11-20 23:18:59 +0800

    2021年11月20日

--- Luogu/P1328/code.cpp
+++ Luogu/P1328/code.cpp
@@ -4,21 +4,25 @@ using namespace std;
 
 int N, Na, Nb;
 int a[MAX], b[MAX];
-int Sa=0, Sb=0;
-int ver[5][5]={{0,0,1,1,0},{1,0,0,1,0},{0,1,0,0,1},{0,0,1,0,1},{1,1,0,0,0}}; 
-int main() {
-	cin>>N>>Na>>Nb;
-	for (int i=0; i<Na; i++) {
-		cin>>a[i];
+int Sa = 0, Sb = 0;
+int ver[5][5] = {{0, 0, 1, 1, 0}, {1, 0, 0, 1, 0}, {0, 1, 0, 0, 1}, {0, 0, 1, 0, 1}, {1, 1, 0, 0, 0}};
+int main()
+{
+	cin >> N >> Na >> Nb;
+	for (int i = 0; i < Na; i++)
+	{
+		cin >> a[i];
 	}
-	for (int i=0; i<Nb; i++) {
-		cin>>b[i];
+	for (int i = 0; i < Nb; i++)
+	{
+		cin >> b[i];
 	}
-	for(int i=0;i<N;i++){
-		Sa+=ver[a[i%Na]][b[i%Nb]];
-		Sb+=ver[b[i%Nb]][a[i%Na]];
-	} 
-	cout<<Sa<<" "<<Sb;
+	for (int i = 0; i < N; i++)
+	{
+		Sa += ver[a[i % Na]][b[i % Nb]];
+		Sb += ver[b[i % Nb]][a[i % Na]];
+	}
+	cout << Sa << " " << Sb;
 	system("pause");
 	return 0;
 }
