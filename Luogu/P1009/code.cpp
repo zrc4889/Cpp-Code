e3c67ef900ee5e0cd3584643a39e87006cc0291d
zrc4889 <zrc4889@163.com>
2021-11-22 22:14:37 +0800

    2021年11月22日

--- Luogu/P1009/code.cpp
+++ Luogu/P1009/code.cpp
@@ -1,1 +1,40 @@
-//TODO:待做
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    int a[10001] = {0}, b[10001] = {0};
+    int n;
+    cin >> n;
+    a[0] = 1, b[0] = 1;
+    for (i = 2; i <= n; i++)
+    {
+        for (j = 0; j < 100; j++)
+        {
+            b[j] *= i;
+        }
+        for (j = 0; j < 100; j++)
+        {
+            if (b[j] > 9)
+            { //这里可有可无
+                b[j + 1] += b[j] / 10;
+                b[j] %= 10;
+            }
+        }
+        for (j = 0; j < 100; j++)
+        {
+            a[j] += b[j];
+            //相加并处理进位
+            if (a[j] > 9)
+            {
+                a[j + 1] += a[j] / 10;
+                a[j] %= 10;
+            }
+        }
+    }
+    for (i = 100; i >= 0 && a[i] == 0; i--)
+        ;
+    for (j = i; j >= 0; j--)
+        cout << a[j];
+}
