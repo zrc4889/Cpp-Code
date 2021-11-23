d1a7d9a6c71a3417c6422e4e29d2a18c35fabfd1
zrc4889 <zrc4889@163.com>
2021-11-23 13:22:17 +0800

    2021年11月23日

--- Luogu/P1011/code.cpp
+++ Luogu/P1011/code.cpp
@@ -7,18 +7,16 @@ int main()
     int a, n, m, x;
     int up[10001], down[10001], now[10001];
     cin >> a >> n >> m >> x;
-    now[1] = now[2] = up[1];
-    up[2] = down[2] = down[3] = x;
-    while (x++ && x >= 3)
+    now[0] = now[1] = up[0] = x;
+    while (x++ && x >= 2 && x!=1001)
     {
         up[x] = up[x - 1] + up[x - 2]; //上车的人数都是前两站上车人数之和
-        down[x] = up[x - 1];           //上车的人数都是前两站上车人数之和
+        down[x] = down[x - 1];         //上车的人数都是前两站上车人数之和
         now[x] = up[x - 2] + down[x - 1];
-        if (now[x - 1] == m)
-        {
-            cout << now[x];
-            break;
-        }
+    }
+    cout<<now[x]<<endl;
+    for (int i=0;i<10;i++){
+        cout<<up[i]<<" "<<down[i]<<" "<<now[i]<<" "<<endl;
     }
     system("pause");
     return 0;
