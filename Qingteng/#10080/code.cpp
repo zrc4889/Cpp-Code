31686f03208fbb51c8b5bea587198f362450ff84
zrc4889 <zrc4889@163.com>
2021-11-23 21:55:10 +0800

    2021年11月23日

--- Qingteng/#10080/code.cpp
+++ Qingteng/#10080/code.cpp
@@ -2,23 +2,27 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     string n;
     int m;
     cin >> n >> m;
 
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= m; ++i)
+    {
         if (n.size() == 0)
             break;
         int j = 0;
 
         // 1239
-        while (j < n.size() - 1 && n[j] <= n[j + 1]) j++;
+        while (j < n.size() - 1 && n[j] <= n[j + 1])
+            j++;
         n.erase(j, 1);
     }
 
     int l = n.size();
-    while (l > 0 && n[0] == '0') {
+    while (l > 0 && n[0] == '0')
+    {
         l--;
         n.erase(0, 1);
     }
