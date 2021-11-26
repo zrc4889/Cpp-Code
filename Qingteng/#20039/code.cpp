1adf51d5cfa41b2b95340500998a19c6925d355d
zrc4889 <zrc4889@163.com>
2021-11-26 22:44:24 +0800

    2021年11月26日打卡

---
+++ Qingteng/#20039/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "I am Xiaoming." << endl
+         << "I come from Hangzhou." << endl
+         << "I like programming." << endl;
+    return 0;
+}
