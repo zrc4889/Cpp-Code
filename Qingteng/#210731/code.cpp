73a0ba3cfc546c3a9be5126a18157e68e324a7af
zrc4889 <zrc4889@163.com>
2021-12-05 15:00:11 +0800

    2021年12月5日打卡

--- Qingteng/#210731/code.cpp
+++ Qingteng/#210731/code.cpp
@@ -4,6 +4,7 @@ int a[1000000]; //数组要定义全局
 int n;
 int ans = 0, sum;
 //前几次CE是因为sum没定义，RE记得看数组全局加题目要求范围和优化
+//CCF最新禁令：不得使用system函数，注释骂人！
 int main()
 {
 	//相向尺取法
@@ -16,35 +17,16 @@ int main()
 	cin >> sum;
 
 	int i = 1, j = n;
-	// for (int i = 0; i < n; i++)
-	// {
-	// 	for (int j = i + 1; j < n; j++)
-	// 	{
-	// 		if (a[i] + a[j] == sum)
-	// 		{
-	// 			ans++;
-	// 		}
-	// 	}
-	// }
 	while (i < j)
 	{
 		if (a[i] + a[j] == sum)
-		{
-			ans++;
-			i++;
-			j--;
-		}
+			ans++, i++, j--;
 		else if (a[i] + a[j] < sum)
-		{
 			i++;
-		}
 		else if (a[i] + a[j] > sum)
-		{
 			j--;
-		}
 	}
-
 	cout << ans; //直接输出ans
-	system("pause");
+	// system("pause");
 	return 0;
 }
