78fbca629a170f3281d67b6ff68e983c2d7cb1e3
zrc4889 <zrc4889@163.com>
2021-12-11 20:56:44 +0800

    2021年12月11日打卡

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [Chinese](https://gitee.com/zrc4889/cpp-code/blob/master/README.md) | [English](https://gitee.com/zrc4889/cpp-code/blob/master/README.en.md)
 
 ### 介绍
-C++学习之路，写代码的仓库啊
+C++学习之路，用于写代码的仓库
 
 ### 联系
 我的Luogu：[传送门](https://www.luogu.com.cn/user/523217)
--- temp.cpp
+++ temp.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-map<string,int>a;
-int p[10001];
+map<string,string>a;
+string p[10001];
 int main(){
     int n;
     cin>>n;
     for (int i=0;i<n;i++){
         string temp;
         cin>>temp;
-        int temp1;
+        string temp1;
         cin>>temp1;
         a[temp] = temp1;
     }
@@ -20,7 +20,7 @@ int main(){
         p[i] = a[temp];
     }
     for (int i=0;i<s;i++){
-        cout<<p[i]<<endl;
+        cout<<p[i]<<" ";
     }
     system("pause");
     return 0;
