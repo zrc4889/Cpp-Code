29c39cf9f469b1be78e0af6632dd8a442d4062bf
zrc4889 <zrc4889@163.com>
2022-01-09 22:37:29 +0800

    2022年1月9日打卡

--- Luogu/CF20A.cpp
+++ Luogu/CF20A.cpp
@@ -1,23 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-string dir;
-int main(){
+string s;
+int main()
+{
+    cin >> s;
+    int l = s.size();
+    while (s[l - 1] == '/' && l >= 1)
+        l--; // 巧用l与字符串下标达成关系
+    if (l == 0)
+        cout << "/" << endl; // 全都是斜杠
+    if (l == 1)
+        cout << s[0] << endl; // 只有一个字符时（可删？）
     int flag = 0;
-    cin>>dir;
-    int len = dir.size();
-    for (int i=0;i<len;i++){
-       if (dir[i] == '/'){
-           if (flag == 0){
-               flag = 1;
-               dir[i] = ' '; // 标记为空，特判输出
-            //    cout<<dir[i];
-           }
-       }else{
-           flag = 0;
-           dir[i] = ' '; // 标记为空
-        //    cout<<dir[i];
-       }
+    for (int i = 0; i < l; ++i)
+    {
+        // 正在一组斜杠中，遇到非斜杠，输出非斜杠字符，结束此组斜杠
+        // 正在一组斜杠中，遇到斜杠，不输出斜杠
+        // 不在一组斜杠中，遇到非斜杠，常规输出
+        // 不在一组斜杠中，遇到斜杠，输出斜杠字符，开始词组斜杠
+
+        if (flag == 1){
+            if (s[i] == '/') continue;
+            if (s[i] != '/') cout<<s[i],flag=0;
+        }else if (flag == 0){
+            if (s[i] == '/') cout<<s[i],flag=1;
+            if (s[i] != '/') cout<<s[i];
+        }
     }
     // system("pause");
-    return 0;
 }
