4fdedd864633d1db05426205e349d7261c160590
zrc4889@163.com <zrc4889@163.com>
2022-01-25 18:45:10 +0800

    2022年1月25日

--- Luogu/T221490.cpp
+++ Luogu/T221490.cpp
@@ -1,58 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
-char a[4][4];
-int ans = 0;
-void check()
+char a[3][3];
+int work1()
 {
-    ans = 0;
-    for (int i = 1; i <= 3; i++)
+    int ans = 0;
+    int p = 0;
+    for (char c = 'A'; c <= 'Z'; ++c)
     {
-        if (a[i][1] == a[i][2] || a[i][1] == a[i][3] || a[i][2] == a[i][3])
-            ans++;
-        if (a[1][i] == a[2][i] || a[2][i] == a[3][i] || a[1][i] == a[3][i])
-            ans++;
-    }
+        // 字符也可以循环
 
-    if (a[1][1] == a[2][2] || a[2][2] == a[3][3] || a[1][1] == a[3][3])
-        ans++;
-    if (a[1][3] == a[2][2] || a[2][2] == a[3][1] || a[1][3] == a[3][1])
-        ans++;
-    cout << ans;
-}
-void check_1()
-{
-    // char temp;
-    ans = 0;
-    for (int i = 1; i <= 3; i++)
-    {
-        // temp = a[i][1];
-        // for (int j=1;j<=3;j++)
-        // {
-        // if (a[i][1] != )
-        //
-        // }
-        if (a[i][1] == a[i][2] && a[i][2] == a[i][3] && a[i][3] == a[i][1])
-            ans++;
-        if (a[1][i] == a[2][i] && a[2][i] == a[3][i] && a[3][i] == a[1][i])
-            ans++;
-    }
-    if (a[1][1] == a[2][2] && a[2][2] == a[3][3] && a[1][1] == a[3][3])
-        ans++;
-    if (a[1][3] == a[2][2] && a[2][2] == a[3][1] && a[1][3] == a[3][1])
-        ans++;
-    cout << ans;
-}
-int main()
-{
-    for (int i = 1; i <= 3; i++)
-    {
-        for (int j = 1; j <= 3; j++)
+        for (int i = 1; i <= 3; ++i)
         {
-            cin >> a[i][j];
+            if (a[i][1] == a[i][2] && a[i][2] == a[i][3] && a[i][3] == a[i][1])
+            {
+                p++;
+            } // 行
+        }
+        for (int i = 1; i <= 3; ++i)
+        {
+            if (a[1][i] == a[2][i] && a[2][i] == a[3][i] && a[3][i] == a[1][i])
+            {
+                p++;
+            }
+        }
+        if (a[1][1] == a[2][2] && a[2][2] == a[3][3] && a[3][3] == a[1][1])
+        {
+            p++;
+        }
+        if (a[1][3] == a[2][2] && a[2][2] == a[3][1] && a[3][1] == a[1][3])
+        {
+            p++;
+        }
+        if (p > 0)
+            ans++;              
+    }
+    return ans;
+}
+bool check(char c1, char c2, char a, char b, char c)
+{
+    if (a != c1 && a != c2)
+        return false;
+    if (b != c1 && b != c2)
+        return false;
+    if (c != c1 && c != c2)
+        return false;
+    if (a != c1 && b != c1 && c != c1)
+        return false;
+    if (a != c2 && b != c2 && c != c2)
+        return false;
+    return true;
+}
+int work2()
+{
+    int ans = 0;
+    for (char c1 = 'A'; c1 <= 'Z'; c1++)
+    {
+        for (char c2 = c1 + 1; c2 <= 'Z'; c2++)
+        {
+            int p = 0;
+            for (int i = 1; i <= 3; ++i)
+                if (check(c1, c2, a[i][1], a[i][2], a[i][3]))
+                    p++;
+            for (int i = 1; i <= 3; ++i)
+                if (check(c1, c2, a[1][i], a[2][i], a[3][i]))
+                    p++;
+            if (check(c1, c2, a[1][1], a[2][2], a[3][3]))
+                p++;
+            if (check(c1, c2, a[1][3], a[2][2], a[3][1]))
+                p++;
+            if (p > 0)
+                ans++;
         }
     }
-    check_1();
-    cout << endl;
-    check();
-    // system("pause");
+    return ans;
+}
+
+int main()
+{
+    for (int i = 1; i <= 3; ++i)
+        for (int j = 1; j <= 3; ++j)
+            cin >> a[i][j];
+    cout << work1() << endl;
+    cout << work2() << endl;
+    return 0;
 }
