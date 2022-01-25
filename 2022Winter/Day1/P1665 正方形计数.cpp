aaa9f740908d2e394a8cedba59500319a642ec30
zrc4889@163.com <zrc4889@163.com>
2022-01-25 16:17:04 +0800

    2022年1月25日

--- 2022Winter/Day1/P1665 正方形计数.cpp
+++ 2022Winter/Day1/P1665 正方形计数.cpp
@@ -2,24 +2,34 @@
 using namespace std;
 int n, ans;
 int x[60000], y[60000], vis[1100][1100], xs[1100][1100];
-int main(){
+int main()
+{
   scanf("%d", &n);
-  for(int i = 1; i <= n; ++i){
+  for (int i = 1; i <= n; ++i)
+  {
     scanf("%d%d", &x[i], &y[i]);
     x[i] = (x[i] + 51) << 1; //防止负数和小数
     y[i] = (y[i] + 51) << 1;
     vis[x[i]][y[i]] = 1;
   }
-  for(int i = 1; i < n; ++i)
-    for(int j = i + 1; j <= n; ++j){
-            int midx = (x[i] + x[j]) / 2;
-            int midy = (y[i] + y[j]) / 2;            
-            int x1 = midx - (midy - y[i]), y1 = midy + (midx - x[i]);
-            int x2 = midx + (midy - y[i]), y2 = midy - (midx - x[i]);
-            if(x1 <= 0) continue; if(x2 <= 0) continue;
-            if(y1 <= 0) continue; if(y2 <= 0) continue; 
-            if(vis[x1][y1] && vis[x2][y2]) ++ans;
-        }
+  for (int i = 1; i < n; ++i)
+    for (int j = i + 1; j <= n; ++j)
+    {
+      int midx = (x[i] + x[j]) / 2;
+      int midy = (y[i] + y[j]) / 2;
+      int x1 = midx - (midy - y[i]), y1 = midy + (midx - x[i]);
+      int x2 =
+      if (x1 <= 0)
+        continue;
+      if (x2 <= 0)
+        continue;
+      if (y1 <= 0)
+        continue;
+      if (y2 <= 0)
+        continue;
+      if (vis[x1][y1] && vis[x2][y2])
+        ++ans;
+    }
   printf("%d\n", ans >> 1);
   return 0;
 }
--- Luogu/T221490.cpp
+++ Luogu/T221490.cpp
@@ -4,25 +4,43 @@ char a[4][4];
 int ans = 0;
 void check()
 {
+    ans = 0;
     for (int i = 1; i <= 3; i++)
     {
         if (a[i][1] == a[i][2] || a[i][1] == a[i][3] || a[i][2] == a[i][3])
             ans++;
-    }
-    // 横向
-    for (int j = 1; j <= 3; j++)
-    {
-        if (a[1][j] == a[2][j] || a[2][j] == a[3][j] || a[1][j] == a[3][j])
+        if (a[1][i] == a[2][i] || a[2][i] == a[3][i] || a[1][i] == a[3][i])
             ans++;
     }
-    //纵向
 
     if (a[1][1] == a[2][2] || a[2][2] == a[3][3] || a[1][1] == a[3][3])
         ans++;
     if (a[1][3] == a[2][2] || a[2][2] == a[3][1] || a[1][3] == a[3][1])
         ans++;
-
-    cout<<ans;
+    cout << ans;
+}
+void check_1()
+{
+    // char temp;
+    ans = 0;
+    for (int i = 1; i <= 3; i++)
+    {
+        // temp = a[i][1];
+        // for (int j=1;j<=3;j++)
+        // {
+        // if (a[i][1] != )
+        //
+        // }
+        if (a[i][1] == a[i][2] && a[i][2] == a[i][3] && a[i][3] == a[i][1])
+            ans++;
+        if (a[1][i] == a[2][i] && a[2][i] == a[3][i] && a[3][i] == a[1][i])
+            ans++;
+    }
+    if (a[1][1] == a[2][2] && a[2][2] == a[3][3] && a[1][1] == a[3][3])
+        ans++;
+    if (a[1][3] == a[2][2] && a[2][2] == a[3][1] && a[1][3] == a[3][1])
+        ans++;
+    cout << ans;
 }
 int main()
 {
@@ -33,6 +51,8 @@ int main()
             cin >> a[i][j];
         }
     }
+    check_1();
+    cout << endl;
     check();
-    system("pause");
+    // system("pause");
 }
