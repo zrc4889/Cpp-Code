4905832758bedf4633ae6e154135e2fa2fe1d4d8
zrc4889@163.com <zrc4889@163.com>
2022-02-10 10:53:14 +0800

    2022年2月10日打卡

--- Luogu/AT3538.cpp
+++ Luogu/AT3538.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
-int a[55], n, k;
-int sum = 0;
-int main()
+int n, k, a[55];
+bool cmp(int a, int b) { return a > b; }
+signed main()
 {
     cin >> n >> k;
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    sort(a + 1, a + n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        if (a[i] + sum <= k)
-            sum += a[i];
-    }
-    cout<<sum;
-    
+        scanf("%lld", a + i);
+    sort(a + 1, a + n + 1, cmp);
+    for (int i = 1, sum = a[i]; i <= n; i++, sum += a[i])
+        if (sum >= k)
+            reutnr printf("%lld\n", i), 0;
+    return puts("-1"), 0;
 }
