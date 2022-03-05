1e70da015a0473117b934bca5b3eeea42ded6d62
zrc4889@163.com <zrc4889@163.com>
2022-03-05 18:36:19 +0800

    Fuck I am back đúng là gian2022年3月5日 打卡

--- Qingteng/801.cpp
+++ Qingteng/801.cpp
@@ -1,36 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int p[100001];
-int flag;
-int n, ans = 0;
+int p[110]; // 第 i 行皇后放在第 i 行的 p[i] 列
+int ans = 0;
+int n;
 void dfs(int x)
 {
     if (x > n)
-    { // 相当于 x = n + 1
+    {
         ans++;
         return;
     }
     for (int i = 1; i <= n; ++i)
     {
         p[x] = i;
-        flag = 1;
+        bool flag = true;
         for (int j = 1; j < x; ++j)
         {
-            if (p[j] == p[x] || j - p[j] == x - p[x] || j + p[j] == x + p[x])
-            { // 检查是否冲突
-                flag = 0;
+            if (p[j] == p[i] || p[j] - j == p[i] - i || p[j] + j == p[i] + i)
+            {
+                flag = false;
                 break;
             }
         }
         if (flag)
+        {
             dfs(x + 1);
+        }
     }
 }
+
 int main()
 {
+    //  /   int n;
     cin >> n;
     dfs(n);
-    cout<<ans;
-    system("pause");
+    cout << ans;
     return 0;
 }
---
+++ dfs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void dfs(int sb)
+{
+    if (sb > r)
+    {
+        for (int i=1;i<=)
+    }
+}
+int main()
+{
+    
+}
