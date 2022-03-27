025252bfdad7626b56fbdfb435a855fe7776e8d8
zrc4889@163.com <zrc4889@163.com>
2022-03-27 13:17:47 +0800

    2022年3月27日

---
+++ Qingteng/10051.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[10001];
+int main()
+{
+    // init
+    int n,x; // n长度 x需要查找的值
+    cin>>n;
+    for (int i=1;i<=n;++i)
+    {
+        cin>>a[i];
+    }
+    cin>>x;
+
+    
+}
