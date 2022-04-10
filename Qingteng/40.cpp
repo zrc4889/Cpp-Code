ee5270d540cfb88317f903eb90456ca73d3bf2c1
zrc4889@163.com <zrc4889@163.com>
2022-04-10 13:56:15 +0800

    2022年4月10日打卡

--- Qingteng/40.cpp
+++ Qingteng/40.cpp
@@ -1,14 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main{
-    int n;
-    cin>>n;
-    for (int i=1;i<=n;i++)
-    {
-        cin>>a[i];
-    }
-    int l=0,r=; // R的值怎么给？
+bool check (int k)
+{
     
+    return 0;
+}
+int main
+{
+    int n, m, L;
+    cin >> L >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    int l = 0, r = L; // R的值怎么给？
+    sort(a + 1, a + n + 1);
+    while (l + 1 < r)
+    {
+        int mid(l + r) / 2;
+        if (check(mid))
+        {
+            l = mid;
+        }
+        else
+        {
+            r = mid;
+        }
+    }
+    if (check(r))
+
+        cout << r;
+
+    else
+        cout << l;
     return 0;
 }
