e73c403e030d589b5e4d6bc8654e1fcd06c28585
zrc4889@163.com <zrc4889@163.com>
2022-07-02 18:01:01 +0800

    2022年7月2日A他coder

--- AtCoder/ABC254/D - Together Square.cpp
+++ AtCoder/ABC254/D - Together Square.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int ans = 0;
+void is(int x)
+{
+    for (int i = 1; i <= x / 2; i++)
+    {
+        if (i * i == x)
+        {   
+            
+            ans++;
+            break;
+        }
+    }
+}
 int main()
 {
     int n;
-    cin>>n;
-    for (int i=1;)
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+
+            if (i == j)
+            {
+                // cout << i << " " << j << endl;
+                ans++;
+                continue;
+            }
+
+            is(i * j);
+        }
+    }
+    cout << ans;
     return 0;
 }
