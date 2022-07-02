d0df54f3cde674b13276052f16a391ac089a8067
zrc4889@163.com <zrc4889@163.com>
2022-07-02 20:34:31 +0800

    2022年7月2日打卡

--- AtCoder/ABC253/D.cpp
+++ AtCoder/ABC253/D.cpp
@@ -17,6 +17,27 @@ signed main()
             sum+=i;
         }
     }
+    cout<<sum;#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int n;
+int a,b;
+
+signed main()
+{
+    cin>>n>>a>>b;
+    int sum=0;
+    for (int i=1;i<=n;i++)
+    {
+        if (i%a!=0&&i%b!=0)
+        {
+            // cout << i << " ";
+            sum+=i;
+        }
+    }
     cout<<sum;
+    return 0;
+}
     return 0;
 }
