d2cc5dcdf1df8e26120848eba8d70d5b095b3a2e
zrc4889@163.com <zrc4889@163.com>
2022-07-02 20:17:12 +0800

    2022年7月2日

---
+++ AtCoder/ABC253/D.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int n;
+int a,b;
+
+signed main()
+{
+    cin>>n>>a>>b;
+    int sum=0;
+    for (int i=1;i<=n;i++)
+    {
+        if (i%a!=0&&i%b!=0)
+        {
+            // cout << i << " ";
+            sum+=i;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
