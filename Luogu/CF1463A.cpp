ac172bb8fb51984cef9db57193f680e12fe24e75
zrc4889@163.com <zrc4889@163.com>
2022-07-10 23:02:24 +0800

    2022年7月10日打卡

--- Luogu/CF1463A.cpp
+++ Luogu/CF1463A.cpp
@@ -3,5 +3,20 @@ using namespace std;
 #define int long long
 signed main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int sum = a + b + c;
+        int temp = sum / 9;
+        if (a < temp || b < temp || c < temp) // 为什么加特判？
+            cout << "NO" << endl;
+        else if (sum % 9 == 0) // 刚好完成增强版
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
 }
