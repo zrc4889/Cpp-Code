1f9424eeeb2e7d79bc988cf59f90af3c5367f2a5
zrc4889@163.com <zrc4889@163.com>
2022-07-29 19:41:36 +0800

    html文档库2022年7月29日内测更新

--- 2022Summer/qtTest/contest/day4/std/number.cpp
+++ 2022Summer/qtTest/contest/day4/std/number.cpp
@@ -1,19 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int MAXN=100005;
-map<long long,int> cnt;
-long long a[MAXN],p[MAXN];
-int ans,T,n,m;
+const int MAXN = 100005;
+map<long long, int> cnt;
+long long a[MAXN], p[MAXN];
+int ans, T, n, m;
 bool flag[MAXN];
-long long rebuild(long long x)
+long long rebuild(long long x) // 去除立方因子
 {
-    for(int i=1;i<=m;++i)
+    for (int i = 1; i <= m; ++i)
     {
-        while(x%p[i]==0)
+        while (x % p[i] == 0)
         {
-            x/=p[i];
+            x /= p[i];
         }
-        if(p[i]>x)
+        if (p[i] > x)
         {
             break;
         }
@@ -24,41 +24,39 @@ int main()
 {
     freopen("number.in", "r", stdin);
     freopen("number.out", "w", stdout);
-    for(long long i=2;i<=1260;++i)
+    for (long long i = 2; i <= 1260; ++i)
     {
-        if(!flag[i])
+        if (!flag[i])
         {
-            p[++m]=i*i*i;
-            for(long long j=i*i;j<=1260;j+=i)
-            flag[j]=true;
-        }
-
+            p[++m] = i * i * i;
+            for (long long j = i * i; j <= 1260; j += i)
+                flag[j] = true;
+        } // 筛出素数
     }
-    ans=0;
+    ans = 0;
     cnt.clear();
-    scanf("%d",&n);
-    for(int i=1;i<=n;++i)
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i)
     {
-        scanf("%lld",&a[i]);
-        a[i]=rebuild(a[i]);
+        scanf("%lld", &a[i]);
+        a[i] = rebuild(a[i]);
         cnt[a[i]]++;
-
     }
-    for(map<long long,int>::iterator i=cnt.begin();i!=cnt.end();++i)
+    for (map<long long, int>::iterator i = cnt.begin(); i != cnt.end(); ++i)
     {
-        if(i->first==1)
+        if (i->first == 1)
         {
             ++ans;
             continue;
         }
         else
         {
-            int Pair=rebuild((i->first)*(i->first));
-            ans+=max(i->second,cnt[Pair]);
-            cnt[Pair]=0;
-            i->second=0;
+            int Pair = rebuild((i->first) * (i->first));
+            ans += max(i->second, cnt[Pair]);
+            cnt[Pair] = 0;
+            i->second = 0;
         }
     }
-    printf("%d\n",ans);
+    printf("%d\n", ans);
     return 0;
 }
