b208f92136c3c89446a747af706ef8bb94044bca
zrc4889@163.com <zrc4889@163.com>
2022-08-27 21:52:16 +0800

    2022年8月27日打卡

---
+++ AtCoder/ABC266/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifdef LOCAL
+    LOCALfo
+#endif
+        ;
+    string a;
+    cin >> a;
+    cout << a[a.length() / 2];
+    return 0;
+}
