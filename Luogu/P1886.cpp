4e9ef8b1ea53e1b5885b547382ae426a49db25f9
zrc4889@163.com <zrc4889@163.com>
2022-11-12 10:56:44 +0800

    2022年11月12日打卡

---
+++ Luogu/P1886.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+priority_queue<int>
+
+int main()
+{
+#ifdef LOCAL
+    LOCALfo
+#endif
+        ;
+
+
+    return 0;
+}
--- Luogu/P2249.cpp
+++ Luogu/P2249.cpp
@@ -1,37 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int n, t;
 const int _ = 1e6 + 10;
-int n, m;
-int q, a[_];
+int a[_];
 int find(int x)
 {
-    int l = 1, r = n;
-    while (l < r)
+    int l, r, mid;
+    l = 1, r = n;
+
+    while (r - l > 1)
     {
-        int mid = (l + r) >> 1;
-        if (a[mid] >= x)
-            r = mid;
+        mid = l + (r - l) / 2;
+        if (a[mid] < x)
+            l = mid;
         else
-            l = mid + 1;
+            r = mid;
     }
     if (a[l] == x)
         return l;
+    else if (a[r] == x)
+        return r;
     else
         return -1;
 }
+
 int main()
 {
 #ifdef LOCAL
     LOCALfo
 #endif
         ;
-    cin >> n >> m;
+    cin >> n >> t;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
-    for (int i = 1; i <= m; i++)
+    while (t--)
     {
-        cin >> q;
-        int ans = find(q);
+        int tmp;
+        cin >> tmp;
+        int ans = find(tmp);
         cout << ans << ' ';
     }
     return 0;
