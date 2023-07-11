79f2127b4a58f2a3f6bd2bd211bb09cc2b825701
zrc4889@163.com <zrc4889@163.com>
2023-07-11 19:24:27 +0800

    2023年7月11日打卡

--- OtherThings/Template/Floyd.cpp
+++ OtherThings/Template/Floyd.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 const int _ = 1001;
-int a[_][_];
+int f[_][_];
 int n;
 
 int main()
@@ -12,12 +12,12 @@ int main()
     
     for (int i = 1; i <= n; ++i)
         for (int j = 1; j <= n; ++j)
-            cin >> a[i][j];
+            cin >> f[i][j];
 
     for (int k = 1; k <= n; ++k)
         for (int i = 1; i <= n; ++i)
             for (int j = 1; j <= n; ++j)
-                f[i][j] = max(f[i][j], f[i][k] + f[k][j]);
+                f[i][j] = min(f[i][j], f[i][k] + f[k][j]);
     
     cout << f[1][n] << el;
 
