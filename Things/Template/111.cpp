e9fc250974f3572ebf77f7d194beba21040456de
zrc4889@163.com <zrc4889@163.com>
2023-10-06 22:33:30 +0800

    2023年10月6日打卡

---
+++ Things/Template/111.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main()
+{
+    cin >> a >> b;
+    
+    for (int i = 1; i <= 101; ++i) cout << a << ' ' << b << '\n';
+
+    return 0;
+}
