7a2d0f329afe95bcc97549f0b08e51de650bb3fe
zrc4889 <zrc4889@163.com>
2023-10-14 00:24:13 +0800

    2023年10月14日打卡

--- Things/Template/111.cpp
+++ Things/Template/111.cpp
@@ -1,13 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b;
+stack<int> s;
+int ans[114514];
+int n, a[114514];
+
+// STL 容器补全很慢
 
 int main()
 {
-    cin >> a >> b;
-    
-    for (int i = 1; i <= 101; ++i) cout << a << ' ' << b << '\n';
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        cin >> a[i];
 
+    for (int i = 1; i <= n; ++i)
+    {
+        while (!s.empty() && !(a[s.top()] <= a[i]))
+            s.pop();
+        if (!s.empty())
+            ans[i] = a[s.top()];
+        s.push(i);
+    }
+    for (int i = 1; i <= n; ++i)
+        cout << ans[i] << '\n';
     return 0;
 }
--- Things/Template/dp01.cpp
+++ Things/Template/dp01.cpp
@@ -1,21 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int _ = 114514;
-int a[_], f[_], t[_];
+
+const int _ = 30;
+
+int v[_], w[_], f[_];
+int n, m;
+int vv, ww, s;
+int cnt = 0;
+
 int main()
 {
-#ifdef LOCAL
-    LOCALfo
-#endif
-        ;
-    int W, n;
-    cin >> W >> n;
-    for (int i = 1; i <= n; i++)
-        cin >> t[i] >> a[i];
-    // f[1] = a[1];
-    for (int i = 1; i <= n; i++)
-        for (int j = W; j >= t[i]; j--)
-            f[j] = max(f[j], f[j - t[i]] + a[i]);
-    cout << f[W] << endl;
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> vv >> ww >> s;
+        for (int j = 1; j <= s; j <<= 1)
+        {
+            ++cnt;
+            v[cnt] = j * vv, w[cnt] = j * ww;
+            s -= j;
+        }
+        if (s)
+        {
+            ++cnt;
+            v[cnt] = s * vv, w[cnt] = s * ww;
+        }
+    }
+
+    for (int i = 1; i <= cnt; ++i)
+        for (int j = w[i]; j <= m; ++j)
+            f[j] = max(f[j], f[j - v[i]] + w[i]);
+
+    cout << f[m];
     return 0;
 }
